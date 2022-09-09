f98fc1115138e4ad20bc7e74006b47fb209cfd4a
Sahil Chachra <sahil.chachra3@live.com>
2022-09-09 19:16:44 +0530

    added comments

--- model.py
+++ model.py
@@ -3,6 +3,14 @@ import torch.nn as nn
 
 class CustomModel(nn.Module):
     def __init__(self, model_name, target_size, pretrained=True, exportable=True):
+        """
+        Parameters :-
+
+        model_name : name of model from Timm
+        target_size : number of classes
+        pretrained : Want ImageNet pretrained model?
+        exportable : Want to export your model to Onnx in future?
+        """
         super(CustomModel, self).__init__()
         self.model = timm.create_model(model_name, num_classes=target_size, pretrained=pretrained, exportable=exportable)
     
